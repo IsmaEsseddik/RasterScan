55359a48b39e85849ae580475936edcf86ea59fd
isma <ismael.esseddik@gmail.com>
2017-08-19 17:02:25 +0200

    http://alp.developpez.com/tutoriels/intelligence-artificielle/reseaux-de-neurones/

--- Numpy_datascience/calculsEtNeurones.py
+++ Numpy_datascience/calculsEtNeurones.py
@@ -34,24 +34,19 @@ def rempuiss(a,b,n):
 """
 
 #-----------Systeme neuronale---------------------
-"""
-def ada_li_ne(x,w): # ADAptive LInear NEuron :equation neurone sans seuil de tolerance
-    assert len(w)== len(x),"nombre d'entrées et nombre de ponderations differents!"
-    p=0
-    for i in range(0,len(w)):
-        p+=w[i]*x[i]
-    return p
-    
-def quadratic_error(y,yd): #y resultat de la fct, yd resultat attendu
-    e=y-yd
-    return (e*e)/2 #la moiti� du carr� de la difference entre theorie et pratique
-"""
+
+def quadratic_error(y, yd):
+    """retourne l'erreur quadratique (la moitié du carré de la difference entre theorie et pratique)avec
+    :y: resultat de la empirique
+    :yd: yd resultat theorique
+    """
+    return (math.sqrt(y - yd))/2
 
 class Neurone:
     """ Objet neurone artificiel a identifiant 'id' (entier positif). """
     # --------------------------- Constructeur ---------------------------
     def __init__(self,id):
-        assert type(id) == 'int' and id>=0, "l'identifiant doit etre un entier positif"
+        assert type(id) is int and id>=0, "l'identifiant doit etre un entier positif"
         self.id = id
 
     # -------------------------- Somme ponderale des influx--------------------------------
@@ -60,19 +55,21 @@ class Neurone:
         assert type(X) == type(W) == list, "les informations et poids doivent etre contenu dans des listes"
         assert len(X) == len(W), "les listes d'information et poids doivent etre de même taille"
         inf = 0
-        for i in range(len(self.W)):
-            assert type(X[i]) in ['int','float'] and 0<=X[i]<=1, "les informations doivent être reel et compris " \
-                                                                 "entre 0 et 1, erreur : "str(i)
-            assert type(W[i]) in ['int','float'], "les poids doivent etre de type numerique, erreur : "str(i)
-            inf += self.X[i]*self.W[i]
+        for i in range(len(W)):
+            assert type(X[i]) in [int, float] and 0<=X[i]<=1, "les informations doivent être reel et compris " \
+                                                                 "entre 0 et 1, erreur : "+str(i)
+            assert type(W[i]) in [int, float], "les poids doivent etre de type numerique, erreur : "+str(i)
+            inf += X[i]*W[i]
         return inf
 
     # --------------- Fonctions g d'activation/transfert du neurone -------------
-    def sigmoide(self,sp):
+    def sigmoide(self,X,W):
+        sp=self.somme_pond(X,W)
         y=1/(1+math.exp(-sp))
         return y
 
-    def heaviside(self,sp):  # neurone a fonction lineaire et n entrées pondérées
+    def heaviside(self,X,W):  # neurone a fonction lineaire et n entrées pondérées
+        sp=self.somme_pond(X,W)
         if (sp>=0):
             return 1
         else:
@@ -81,7 +78,7 @@ class Neurone:
     def sOUple(self,x1,x2):
         """ retourne un booleen sur un test logique OU avec seuil de signal (1/2)  """
         assert 0<=x1<=1 and 0<=x2<=1 ,"les deux entrées doivent être reel compris entre 0 et 1"
-        if sp >= seuil:
+        if x1+x2 >= 0.5:
             return 1
         else:
             return 0
@@ -94,35 +91,59 @@ class Neurone:
         else:
             return 0
 
-def RdN(X, xWp, P ):
-    """Reseaux de neurone avec X ndarray contenant les informations, xWp ndarray contenant les poids, P liste d'objects
-    de classe neurone
-    doit verifier que
-    len(X) == len(matrice en ligne )
-    len(P) == len(matrice en colone )
-    matrice est de dtype numeric
-    """
 
-def matrex(n,p):
-    inf = [-1]
-    for i in range(n):
-        inf.append(random.choice(range(1,10)))
-    neurones = []
-    for p in range(p):
-        neurones.append(Neurone(np.ndarray())
+class Perceptron:
+    """ Réseau de neurones monocouche"""
+    def __init__(self,nb,mode='ff'):
+        """Constructeur de perceptron, prend en entrée un nombre de neurone ansi que le type
+        'feed-forward'(normaux) ou 'récurrents'(alimentent leurs entrées avec leurs sorties)"""
+        assert type(nb) is int and nb>0, 'nb doit etre entier positif'
+        self.rdn = []
+        for i in range(nb):  # on va creer une liste de 5 objets de la class neurones, identifié par un numero de 0a4
+            self.rdn.append(Neurone(i))
+
+    def rdn_activ(self,X, M):
+        """Fonction d'activation du reseaux de neurone, avec
+        :X: liste d'informations,
+        :M: matrice 'ndarray' contenant les poids info/neurone,
+        """
+        assert type(M) is np.ndarray and M.ndim==2, "ce n'est pas une matrice 'ndarray' en 2 dimension"
+        assert np.issubdtype(M.dtype, np.number), "La matrice n'est pas integralement de type numerique ?"
+        assert len(X)== len(M), "le nombre d'information ne correspond pas a la matrice"
+        assert len(self.rdn) == len(M[0])
+        list_activ=[]
+        for neu in self.rdn:
+            W = []
+            for i in range(len(X)):
+                W.append(int(M[i][neu.id]))
+            list_activ.append(neu.heaviside(X,W))
+        return list_activ
+
+
+    # -----------------------Apprentissage par descente de gradient---------------------------
+"""def dw(n):
+    dw=[]
+    for i in range(n)
+        dw.append(0)
+    return dw
+def exemples(N):
+    e=[]
+    for k in range(N):
+        e.append(random.randrange())
+def (X,W,N)"""
+
 
 
 if __name__ == "__main__":
-    w=np.array([1,2,1,2,2,1,2])
-    x=np.array([1,6,4,7,8,2,12],dtype=int)
-    a = Neurone(x,w)
-    x1 = 0.2
-    x2 = 0.73
-    x3 = 0
-    x4 = 1
-    print(fonction_XOR(x2, x2))
-    print(fonction_XOR(x4, x2))
-    matrex(5,6)
-
-
-
+    W=[1,2,1,2,2,1,2]
+    X=[0.1,0.6,0.4,0.7,0.8,0.2,0.12]
+    a = Neurone(0)
+    x1,x2,x3, x4 = 0.2, 0.73, 0, 1
+    print(a.XOR(x2, x2))
+    print(a.sOUple(x4, x2))
+    print(a.heaviside(X,W))
+    print(a.sigmoide(X,W))
+    P=Perceptron(5)
+    M = np.random.randint(-2, 4, ((len(X), len(P.rdn))))  # Creation d'une matrice de ponderation neuroneXinfo aleatoire
+    print(M, type(M))
+    print(P.rdn_activ(X,M))
