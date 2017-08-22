7910ad3bb258b2eb1afdd4c1fd5b6adf81abf8a6
isma <ismael.esseddik@gmail.com>
2017-08-22 17:01:03 +0200

    http://alp.developpez.com/tutoriels/intelligence-artificielle/reseaux-de-neurones/

--- Numpy_datascience/calculsEtNeurones.py
+++ Numpy_datascience/calculsEtNeurones.py
@@ -1,8 +1,12 @@
 # coding: utf-8
-import random
+
+# Ce script est l'application theorique des elements en provenance de cet page:
+# http://alp.developpez.com/tutoriels/intelligence-artificielle/reseaux-de-neurones/
+
+# import random
 import math
 import numpy as np
-import matplotlib as plt
+# import matplotlib as plt
 """
 #------- test palindrome-------------
 txt='je suis inverse'
@@ -33,21 +37,13 @@ def rempuiss(a,b,n):
 # -------------------------------------------------------------------------
 """
 
-#-----------Systeme neuronale---------------------
-
-def quadratic_error(y, yd):
-    """retourne l'erreur quadratique (la moitié du carré de la difference entre theorie et pratique)avec
-    :y: resultat de la empirique
-    :yd: yd resultat theorique
-    """
-    return (math.sqrt(y - yd))/2
-
+# -----------Système neuronale---------------------
 class Neurone:
-    """ Objet neurone artificiel a identifiant 'id' (entier positif). """
+    """ Objet neurone artificiel doté d'un identifiant 'id' (entier positif). """
     # --------------------------- Constructeur ---------------------------
-    def __init__(self,id):
-        assert type(id) is int and id>=0, "l'identifiant doit etre un entier positif"
-        self.id = id
+    def __init__(self, identifiant):
+        assert type(identifiant) is int and identifiant >= 0, "l'identifiant doit etre un entier positif"
+        self.id = identifiant
 
     # -------------------------- Somme ponderale des influx--------------------------------
     def somme_pond(self, X, W):
@@ -83,13 +79,51 @@ class Neurone:
         else:
             return 0
 
-    def XOR(self,x1,x2):
+    def neurxor(self,x_1,x_2):
         """ retourne une valeur selon un test logique OU, """
-        assert 0<=x1<=1 and 0<=x2<=1 ,"les deux entrées doivent être reel compris entre 0 et 1"
-        if ((x1 ==1 and x2 != 1) or (x1 !=1 and x2 == 1) ):
+        assert 0<=x_1<=1 and 0<=x_2<=1 ,"les deux entrées doivent être reel compris entre 0 et 1"
+        if ((x_1 ==1 and x_2 != 1) or (x_1 !=1 and x_2 == 1) ):
             return 1
         else:
             return 0
+
+    # -----------------------Apprentissage par descente de gradient---------------------------
+    def quadratic_error(self, y, yd,alpha=0.5):
+        """retourne l'erreur quadratique (la moitié du carré de la difference entre theorie et pratique)avec
+        :y: resultat de la empirique
+        :yd: yd resultat theorique
+        """
+        return (math.sqrt(y-yd)) / 2
+
+    """
+    def dwnlearn_grad(self,XYk,W,alpha=0.5):
+        Fonction qui retourne la liste des n poids modifié,avec
+        :XYk: une liste de N exemples (tuple k) possedant une liste d'informations et le resultat attendu.
+        :W: liste des n poids actuels
+        dw=[] #on creer une liste de poids
+        print(dw)
+        for i in range(0,len(w)): # on les initialise tous a 0
+            dw[i]=0
+        for e in XYk: # pour chaque exemple
+            sk=self.sigmoide(e[0],W) #calcul du resultat sk avec les poids actuels
+            for i in dw:
+                dw[i]+= alpha*(e[1]-sk)*e[0][i]
+        print(dw)
+        for i in dw:
+            W[i]+=i
+        # Fonction non testé
+    """
+
+    def learn_Widrow_Hoff(self,XYk,W,alpha=0.5):
+        """Fonction qui retourne la liste des n poids modifié,avec
+        :XYk: une liste de N exemples (tuple k) possedant une liste d'informations et le resultat attendu.
+        :W: liste des n poids actuels
+        """
+        for e in XYk: # pour chaque exemple
+            sk=self.sigmoide(e[0],W) #calcul du resultat sk avec les poids actuels
+            for i in W:
+                i+= alpha*(e[1]-sk)*e[0][i]
+        return W  # a essayer
 
 
 class Perceptron:
@@ -107,9 +141,9 @@ class Perceptron:
         :X: liste d'informations,
         :M: matrice 'ndarray' contenant les poids info/neurone,
         """
-        assert type(M) is np.ndarray and M.ndim==2, "ce n'est pas une matrice 'ndarray' en 2 dimension"
+        assert type(M) is np.ndarray and M.ndim == 2, "ce n'est pas une matrice 'ndarray' en 2 dimension"
         assert np.issubdtype(M.dtype, np.number), "La matrice n'est pas integralement de type numerique ?"
-        assert len(X)== len(M), "le nombre d'information ne correspond pas a la matrice"
+        assert len(X) == len(M), "le nombre d'information ne correspond pas a la matrice"
         assert len(self.rdn) == len(M[0])
         list_activ=[]
         for neu in self.rdn:
@@ -119,27 +153,12 @@ class Perceptron:
             list_activ.append(neu.heaviside(X,W))
         return list_activ
 
-
-    # -----------------------Apprentissage par descente de gradient---------------------------
-"""def dw(n):
-    dw=[]
-    for i in range(n)
-        dw.append(0)
-    return dw
-def exemples(N):
-    e=[]
-    for k in range(N):
-        e.append(random.randrange())
-def (X,W,N)"""
-
-
-
 if __name__ == "__main__":
     W=[1,2,1,2,2,1,2]
     X=[0.1,0.6,0.4,0.7,0.8,0.2,0.12]
     a = Neurone(0)
-    x1,x2,x3, x4 = 0.2, 0.73, 0, 1
-    print(a.XOR(x2, x2))
+    x1, x2, x3, x4 = 0.2, 0.73, 0, 1
+    print(a.neurxor(x2, x2))
     print(a.sOUple(x4, x2))
     print(a.heaviside(X,W))
     print(a.sigmoide(X,W))
